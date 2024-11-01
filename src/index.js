3e651ab7fd9ba3e78a83538902eefbf385ca4ef6
LoftySky <aleksandrgrinkevich135@gmail.com>
2024-11-01 11:42:31 +0300

    Сделал ошибку и секрет

--- src/index.js
+++ src/index.js
@@ -69,6 +69,11 @@ app.get('/secret', (req, res) => {
     res.render('secret', { title: 'SECRETS' });
 });
 
+// Маршрут для неизвестных запросов (должен быть последним маршрутом)
+app.use((req, res, next) => {
+    res.status(404).render('page404', { title: 'Page Not Found' });
+});
+
 /*
 app.use("/login", function(request, response){
     response.render("login");
